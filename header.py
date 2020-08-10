794339e3b0e6efee87be9d919e4468a509fb3af1
garykwan44 <68102154+garykwan44@users.noreply.github.com>
2020-08-10 17:35:51 +0800

    Delete header.py

--- header.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-token = '1343142606:AAG7_HsYBvPcT_UyGXQ2ytkaTCujBM4dumo'
-# https://api.telegram.org/bot1343142606:AAG7_HsYBvPcT_UyGXQ2ytkaTCujBM4dumo/setWebhook?url=https://f2375c15c11f.ngrok.io
